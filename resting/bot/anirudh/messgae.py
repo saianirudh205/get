81f6a6ceeee134d7f0047d6e9aa271bfc9caf10d
Sai Anirudh <62623048+saianirudh205@users.noreply.github.com>
2020-03-25 21:42:12 +0530

    Add files via upload

---
+++ resting/bot/anirudh/messgae.py
@@ -0,0 +1,19 @@
+import json as js
+import os
+os.chdir(r'C:\Users\Admin\OneDrive\django\resting\bot\anirudh')
+def get(ani,a,b):
+        print(0)
+        with open('jack.json','r') as f:
+            ani=js.load(f)
+        print(1)
+        with open('jack.json','w') as f:
+            f.close()
+        print(2)
+        ani['doc'].append({a:b})
+        with open('jack.json','w') as f:
+            js.dump(ani,f)
+        return ani
+def clear():
+    with open('jack.json','w') as f:
+            js.dump({"doc":[]},f)
+            f.close()
